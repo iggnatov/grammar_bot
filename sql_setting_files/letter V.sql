150f300949ccf1b59767b13801ae73cffe08c89e
iggnatov <iggnatov@gmail.com>
2023-02-02 18:17:03 +0300

    версия 3.1

---
+++ sql_setting_files/letter V.sql
@@ -0,0 +1,5 @@
+INSERT INTO words (word, gap_index, mistakes) VALUES ('вакуум',4, 0);
+INSERT INTO words (word, gap_index, mistakes) VALUES ('вакханалия',1, 0);
+INSERT INTO words (word, gap_index, mistakes) VALUES ('валенки',3, 0);
+INSERT INTO words (word, gap_index, mistakes) VALUES ('варежки',4, 0);
+INSERT INTO words (word, gap_index, mistakes) VALUES ('ветеран',1, 0);
