cf86a68aaceb87c732906830529f2fdef9049da5
iggnatov <iggnatov@gmail.com>
2023-02-13 18:15:11 +0300

    версия 3.1

--- grammar.py
+++ grammar.py
@@ -5,14 +5,29 @@ import json
 
 class WordSet:
 
-    def __init__(self, set_name):
-        self.set_name = set_name
+    def __init__(self):
+        self.set_name = self.choose_word_set_name()
+        self.set_status = self.choose_set_status()
+        self.set_file_name = self.choose_word_set_to_add()
 
+    @staticmethod
+    def choose_word_set_name():
+        # add verify for rus letters and digits
+        set_name = input('Type the name of the word set, that will be displayed for user:\n')
+        return set_name
+
+    @staticmethod
+    def choose_set_status():
+        # add verify on or off
+        set_status = input('Choose the status of your set:\n'
+                           'Type \'1\' - to set ACTIVE status\n'
+                           'Type \'2\' - to turn your set off - INACTIVE status\n')
+        return 'ACTIVE' if set_status == '1' else 'INACTIVE'
 
     @staticmethod
     def choose_word_set_to_add():
         # The path for listing items
-        path = '/home/admin/grammar_bot/word_sets/'
+        path = '/Users/iggnatov/Documents/dev/grammar_bot/word_sets'
 
         # The list of items
         files = os.listdir(path)
@@ -23,87 +38,96 @@ class WordSet:
         for filename in files:
             print(filename)
 
-        return '/home/admin/grammar_bot/word_sets/' + input()
-
-    @staticmethod
-    def choose_word_set_name():
-        # add verify for rus letters and digits
-        print('Choose (Type) the name of a word set for user to display:')
-        return input()
-
-    @staticmethod
-    def choose_set_status():
-        # add verify on or off
-        print('Choose (Type) status of your set (on or off):')
-        return input()
+        set_file_name = input()
+        return set_file_name
 
     def create(self):
-        with open('/home/admin/grammar_bot/sql_setting_files/config_file.txt', 'r') as json_file:
+        with open('/Users/iggnatov/Documents/dev/grammar_bot/sql_setting_files/config_file.txt', 'r') as json_file:
             j_data = json.load(json_file)
 
         try:
             connection = psycopg2.connect(user=j_data['user'], password=j_data['password'],
-                                          host=j_data['host'], port='5432', database=j_data['dbname'])
+                                          host=j_data['host'], port='5433', database=j_data['dbname'])
             cursor = connection.cursor()
             # print(connection.get_dsn_parameters(), "\n")
             cursor.execute("SELECT version();")
             record = cursor.fetchone()
             print("You\'re connected to - ", record, "\n")
 
-
-            file_name = self.choose_word_set_to_add()
-            set_name = self.choose_word_set_name()
-            set_status = self.choose_set_status()
-
             cursor.execute(f"""INSERT INTO word_sets (set_file_name, set_status, set_name) 
-                            VALUES ({file_name}, {set_status}, {set_name});""")
-            record = cursor.fetchone()
+                            VALUES ('{self.set_file_name}', '{self.set_status}', '{self.set_name}');""")
+            # record = cursor.fetchone()
             print(record)
+            connection.commit()
 
-            cursor.execute(f"""SELECT * FROM word_sets 
-                            WHERE file_name == {file_name};""")
+            cursor.execute(f"""SELECT id FROM word_sets 
+                            WHERE set_file_name = '{self.set_file_name}';""")
             record_id = cursor.fetchone()
-            print(record_id)
+            print('record_id: ', record_id[0])
 
 
-            f = open(file_name, 'r')
-            print(f"File {file_name} opened")
+            f = open('/Users/iggnatov/Documents/dev/grammar_bot/word_sets/' + self.set_file_name, 'r')
+            print(f"File \'{self.set_file_name}\' opened")
 
             try:
+                # making a word
                 word_list = f.readlines()
                 word_list = [s_word.strip() for s_word in word_list]
                 print('Word_list has been read')
-                # i = 0
-                # for each_word in word_list[0:]:
-            #         i += 1
-            #         print(f"Taking the {i}-element - {each_word}")
-            #         # making a word
-            #         word = class_word.Word(each_word, self.set_name)
-            #
-            #         command = word.get_word_full_command()
-            #         print(f"Command to be used:\n{command}")
-            #
-            #         cursor.execute(command)
-            #         print(cursor)
-            #         connection.commit()
-            #         print('Connection committed')
-            #         # count = cursor.rowcount
-            #         print("Line was successfully added")
-            #     print(i, " lines were successfully added")
+                i = 0
+                for each_word in word_list[0:]:
+                    i += 1
+                    print(f"Taking the {i}-element - {each_word}")
+                    # making a word
+                    word_without_brackets = each_word.replace('[', '').replace(']', '')
+                    gap_index = each_word.index('[')
+                    word_command = f"""INSERT INTO words (word, gap_index) 
+                            VALUES ('{word_without_brackets}', {gap_index});"""
+                    print(f"Command to be used:\n{word_command}")
+
+                    cursor.execute(word_command)
+                    print(cursor)
+                    connection.commit()
+                    print('Connection committed')
+
+                    # making a relation between word and set
+                    # ...
+                    cursor.execute(f"""SELECT id FROM words
+                    WHERE word = '{word_without_brackets}';""")
+                    word_id = cursor.fetchone()
+                    print('word_id: ', word_id[0])
+
+                    relation_command = f"""INSERT INTO rel_words_sets (word_id, set_id) 
+                    VALUES ({word_id[0]}, {record_id[0]});"""
+                    print(f"Command to be used:\n{relation_command}")
+
+                    cursor.execute(relation_command)
+                    print(cursor)
+                    connection.commit()
+                    print('Connection committed')
+
+                print(i, " lines were successfully added")
+
+
 
             finally:
-                print(f"File {file_name} closed")
+                print(f"File {self.set_file_name} closed")
                 f.close()
 
         except psycopg2.errors.UniqueViolation:
-            print(f'Such a word already exists')
+            print("""Value is already exist.\n
+            Check up input values""")
 
         except (Exception, Error) as error:
-            print("Ошибка при работе с PostgreSQL", error)
+            print("Error working with PostgreSQL", error)
 
         finally:
             # print('finally')
-            if connection:
-                cursor.close()
-                connection.close()
-                print("Connection to PostgreSQL closed")
+            # if connection:
+            cursor.close()
+            connection.close()
+            print("Connection to PostgreSQL closed")
+
+
+
+
--- main.py
+++ main.py
@@ -7,8 +7,7 @@ if __name__ == '__main__':
                     'Type \'A\' to add a word_set \n'
                     'Type \'Q\' to quit \n')
         if us == 'A':
-            input_name = input('...adding a new word_set \nType the set_name, please! \n')
-            word_set = grammar.WordSet(input_name)
+            word_set = grammar.WordSet()
             word_set.create()
 
         elif us == 'Q':
